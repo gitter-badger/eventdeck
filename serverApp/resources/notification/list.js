6050c0aa1098b2ca519a32a45980aba5692e0335
Diogo Pais <dpais11@gmail.com>
2014-09-26 00:17:41 +0100

    trying to reduce notifications load

--- serverApp/resources/notification/list.js
+++ serverApp/resources/notification/list.js
@@ -11,6 +11,16 @@ function list(request, reply) {
       return reply({error: 'There was an error getting all the notifications.'});
     }
 
+    result = result.sort(function (o1, o2) {
+    	return Date.parse(o2.posted) - Date.parse(o1.posted);
+    });
+
+    var i = 0;
+
+    result = result.filter(function (o) {
+    	return (o.targets.indexOf(request.auth.credentials.id) !== -1 && o.unread.indexOf(request.auth.credentials.id) !== -1) || i++ < 50;
+    });
+
     reply(result);
   });
 
